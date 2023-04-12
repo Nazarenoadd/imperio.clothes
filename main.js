1c5c10987b02d3dee8c90b3a33c9959c7d5b1729
Nazarenoadd <Nazareno.roldan18@gmail.com>
2023-04-12 00:02:50 -0300

    se agrego proyecto

---
+++ main.js
@@ -0,0 +1,4 @@
+function saludar() {
+    let nombre = document.getElementById("nombre").value;
+    document.getElementById("saludo").innerHTML = "¡Hola, " + nombre + "! gracias por visitarnos, te invitamos a que nos sigas en nuestras redes asi estas al tanto de lo ultimo en contenido";
+  }
